53ef26a06ae8492f2570d3fba3d4cc73f158f18b
joe lee <as955133@gmail.com>
2018-11-30 08:56:47 +0800

    C#氣泡排序法練習

--- 20181130_練習/Program.cs
+++ 20181130_練習/Program.cs
@@ -6,7 +6,24 @@ namespace _20181130_練習
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int temp = 0;
+            int[] Ary = new int[] { 12, 9, 6, 2, 4, 1, 11, 8, 3, 5, 7, 10, 13 };
+
+            for(int i =0; i < Ary.Length; i++)
+            {
+                for(int k = i + 1; k < Ary.Length; k++)
+                {
+                    if(Ary[i] > Ary[k])
+                    {
+                        temp = Ary[k];
+                        Ary[k] = Ary[i];
+                        Ary[i] = temp;
+                    }
+                }
+                Console.WriteLine($"{Ary[i]}");
+            }
+            Console.Read();
+
         }
     }
 }
