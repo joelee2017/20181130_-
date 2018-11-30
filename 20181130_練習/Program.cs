561e61d2248c8c5fccc014561eb1462be891206c
joe lee <as955133@gmail.com>
2018-11-30 08:13:02 +0800

    加入專案檔案。

---
+++ 20181130_練習/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20181130_練習
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
